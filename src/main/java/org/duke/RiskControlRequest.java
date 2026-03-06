4eed808a87cc76997ce515595c2d993c99555c7c
dukeMacMini <422931278@qq.com>
2026-03-06 18:01:30 +0800

    添加风控请求实体类和lombok依赖

---
+++ src/main/java/org/duke/RiskControlRequest.java
@@ -0,0 +1,13 @@
+package org.duke;
+
+import lombok.Data;
+import java.util.Map;
+import java.util.Set;
+
+@Data
+public class RiskControlRequest {
+    private Map<String, Object> inputParams;
+    private Set<String> indicators;
+    private Long businessId;
+    private Long riskControlId;
+}
